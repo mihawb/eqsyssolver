0646404260325b542fd4a1b9b5f751c858bb1638
MarvelousMarcin <01159356@pw.edu.pl>
2021-01-13 19:53:48 +0100

    Sprawdzanie parametrow wejsciowych

--- src/main.c
+++ src/main.c
@@ -7,8 +7,8 @@
 
 int main(int argc, char ** argv) {
 	int res;
-	Matrix * A = readFromFile(argv[1]);
-	Matrix * b = readFromFile(argv[2]);
+	Matrix * A = readFromFile(argc > 1 ? argv[1] : "A");
+	Matrix * b = readFromFile(argc > 2 ? argv[2] : "b");
 	Matrix * x;
 
 	if (A == NULL) return -1;
