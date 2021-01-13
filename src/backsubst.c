4bce65d048b17c6799e8422c986f1b45af30c8b1
MarvelousMarcin <01159356@pw.edu.pl>
2021-01-13 19:21:41 +0100

    Implementacja Podstawienia wstecznego v.1

--- src/backsubst.c
+++ src/backsubst.c
@@ -4,19 +4,20 @@
  * Zwraca 1 - błąd dzielenia przez 0 (element na diagonali = 0)
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
-int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				int i;
-				for (i =0; i < x->r; i++) {
-					x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+int backsubst(Matrix *x, Matrix *mat, Matrix *b) 
+{
+	int k,w;
+	double sum;
+	for(w = mat->r-1;w>=0;w--)
+	{
+		sum = 0;	
+		for(k = w+1;k<mat->c;k++)
+			sum += mat->data[w][k] * x->data[k][0];
+	
+		x->data[w][0] = (b->data[w][0] - sum) / mat->data[w][w];
+		
+	}
+	return 0;
 }
 
 
