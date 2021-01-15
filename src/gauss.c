cd0737da3c41c15f18bc6145b8d491c10f8ab3ab
Marcin Maciąg <73672172+MarvelousMarcin@users.noreply.github.com>
2021-01-15 13:55:34 +0100

    Gauss v.2

--- src/gauss.c
+++ src/gauss.c
@@ -1,49 +1,53 @@
 #include "gauss.h"
-
-int abs(int x) {
-    if (x < 0)
-        return -x;
-    return x;
-}
-
-void rowOrderModifier(Matrix * m, int x, int y) {
-    double *tmp = m->data[x];
-    m->data[x] = m->data[y];
-    m->data[y] = tmp;
-}
-
 /**
  * Zwraca 0 - elimnacja zakonczona sukcesem
  * Zwraca 1 - macierz osobliwa - dzielenie przez 0
  */
-int eliminate(Matrix *mat, Matrix *b){
-    int ir, jr, jc;
-    int i;
-    double constInR;              
-    double rMaxElem;    // do poszukiwania elementu glownego
-    int rMaxIndex;      // i numeru wiersza w ktorym sie znajduje
-
-    for (ir = 0; ir < mat->r; ir++) {
-        rMaxElem = abs(mat->data[ir][ir]);          // [ir][ir] bo to element diagonalny w danym wierszu
-        for (i = ir; i < mat->r; i++) {             // poszukiwanie elementu glownego w kolumnie
-            if (abs(mat->data[i][ir]) > rMaxElem) {
-                rMaxElem = mat->data[i][ir];
-                rMaxIndex = i;
-            }
+int eliminate(Matrix *mat, Matrix *b)
+{
+        int c,r;
+        double wsp;
+        int j,i,p;
+        double tmp;;
+        for(c=0;c<mat->c-1;c++)
+        {
+                for(c=0;c<mat->c-1;c++)
+                {
+                        /*Wybieramy element główny - Równanie posiadające największą co do modułu wartośc w kolumnie*/
+                        int rmax = c;
+                        for(i=c+1;i<mat->r;i++)
+                        {
+                                if(fabs(mat->data[i][c])>fabs(mat->data[rmax][c]))
+                                        rmax = i;
+                        }
+                        /*Podmieniamy równania*/
+                        if(rmax != c)
+                        {
+                                for(p=c;p<mat->c;p++)
+                                {
+                                        tmp = mat->data[rmax][p];
+                                        mat->data[rmax][p] = mat->data[c][p];
+                                        mat->data[c][p] = tmp;
+                                }
+                                tmp = b->data[rmax][0];
+                                b->data[rmax][0] = b->data[c][0];
+                                b->data[c][0] = tmp;
+                        }
+                        /*Zerujemy kolumnę*/
+                        for(r=c+1;r<mat->r;r++)
+                        {
+				if(data[c][c] == 0)
+					return 1;
+                                wsp = mat->data[r][c]/mat->data[c][c];
+                                for(j=c;j<mat->c;j++)
+                                {
+                                        mat->data[r][j] -= mat->data[c][j]*wsp;
+                                }
+                                b->data[r][0] -= b->data[c][0]*wsp;
+                        }
+                }
         }
-        if (rMaxElem > abs(mat->data[ir][ir])) {    // jesli element glowny nie znajduje sie w ir, zamien wiersze 
-            rowOrderModifier(mat, ir, rMaxIndex);
-            rowOrderModifier(b, ir, rMaxIndex);
-        }
-
-        for (jr = ir + 1; jr < mat->r; jr++) {                      // dla wszystkich wierszy pod diagonala (pod w. aktualnie okreslonym przez ir)
-            constInR = mat->data[jr][ir] / mat->data[ir][ir];       // o ta stala trzeba przemnozyc kazdy element w wierszu wiec dla jasnosci dalej uzywa sie zmiennej
-            for (jc = ir; jc < mat->c; jc++)                        // dla wszystkich elementow wiersza za diagonala
-                mat->data[jr][jc] -= constInR * mat->data[ir][jc];
-            b->data[jr][0] -= constInR * b->data[ir][0];
-        }
-    }
-    
-	return 0;
+                return 0;
 }
+
 
