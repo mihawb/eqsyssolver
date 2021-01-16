8a84950d16bd36282e41f4f1bd9100d5c7abf0af
zvzia <gaik.zuza@gmail.com>
2021-01-16 16:58:08 +0100

    poprawa funkcji testujacej

--- src/test.c
+++ src/test.c
@@ -1,16 +1,29 @@
 #include "mat_io.h"
 #include "test_1.h"
+#include <math.h>
+
+double my_round(double x, unsigned int digits) {
+	if (digits > 0) {
+		return my_round(x * 10.0, digits - 1) / 10.0;
+	}
+	else {
+		return round(x);
+	}
+}
 
 void check(char* solution, Matrix* x) {
 
 	Matrix* sol = readFromFile(solution);
 
-	int i, j, sol_i, x_i;
+	int i, j;
+	double sol_r, x_r;
 	for (i = 0; i < sol->r; i++) {
 		for (j = 0; j < sol->c; j++) {
-			sol_i = sol->data[i][j] * 10000000;         //jak tego nie bylo to nie dzialalo bo wynik roznil sie na jakims milionowym miejscu po przecinku, 
-			x_i = x->data[i][j] * 10000000;             //pomimo ze wynik by³ np 2,123 to ten obliczony w programie by³ 2,123000000000000000000000000000000012 
-			if (sol_i != x_i) {
+
+			sol_r = my_round(sol->data[i][j], 5);
+			x_r = my_round(x->data[i][j], 5);
+
+			if (sol_r != x_r) {
 				printf("Zly wynik");
 				return 1;
 			}
